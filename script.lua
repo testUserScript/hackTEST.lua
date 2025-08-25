b83d7c51d3c88fd5124fa1588bfa359fe9e2eb58
testUserScript <selascou2024@gmail.com>
2025-08-24 22:06:44 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -9,11 +9,13 @@ local CloseBtn = Instance.new("TextButton")
 CloseBtn.Size = UDim2.new(0, 100, 0, 50)
 CloseBtn.Position = UDim2.new(0.5, 0, 0.5, 0)
 CloseBtn.Text = "test"
+CloseBtn.AnchorPoint = Vector2.new(0.5, 0.5)
 CloseBtn.BackgroundColor3 = Color3.fromRGB(200, 50, 50)
 CloseBtn.TextColor3 = Color3.fromRGB(255,255,255)
 CloseBtn.Parent = screenGui
 CloseBtn:MouseButton1Click:Connect(function()
 	humanoid.WalkSpeed = 80
 end)
+
 
 
